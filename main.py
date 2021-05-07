33239d321958bd6ae2f511f7a5bf0cef93a7eb2a
reinersr <78329332+reinersr@users.noreply.github.com>
2021-05-07 13:33:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 work_edges = []
 # out_edges = []
@@ -9,7 +10,7 @@ def find_matching():
 
 	out_edges = []
 
-	for i in work_edges[:round(len(work_edges)/4)+2]:
+	for i in work_edges[:round(len(work_edges)/2)+2]:
 		if(check(i, out_edges)):
 			out_edges.append(i)
 			if(len(out_edges)%100==0):
@@ -26,18 +27,20 @@ def check(edge, out_edges):
 			return False
 	return True
 
-def print_out(output):
-	fout = open('C:/Users/robbo/Desktop/output.csv', 'w')
-	for i in output:
-		fout.write(i[0])
-		fout.write(',')
-		fout.write(i[1])
-		fout.write('\n')
-	fout.close()
+# def print_out(output):
+	# fout = open('C:/Users/robbo/Desktop/output.csv', 'w')
+	# for i in output:
+		# fout.write(i[0])
+		# fout.write(',')
+		# fout.write(i[1])
+		# fout.write('\n')
+	# fout.close()
 
 def main():
-	fin = open('C:/Users/robbo/Documents/GitHub/Data_proc/final_project/musae_ENGB_edges.csv', "r")
-
+	# fin = open(sys.argv[0], "r")
+	fin = open(sys.argv[1], "r")
+	fout = open(sys.argv[2], 'w')
+	
 	for i in fin.readlines():
 		work_edges.append(i.strip().split(','))
 	
@@ -45,10 +48,18 @@ def main():
 	print('Original length:\t', len(work_edges))
 	for i in range(100):
 		temp = find_matching()
+		fout.write("Run: " + str(i) + '\n')
+		fout.write(str(temp) + '\n')
+		fout.write(str(len(temp)) + '\n')
+		fout.write('\n')
+		fout.flush()
 		if(len(temp)>len(output)):
 			output = temp
-	print_out(output)
+	fout.write('------------------------------------------------------\n')
+	fout.write(str(output) + '\n')
+	fout.write(str(len(output)) + '\n')
 	print('Length:\t', len(output))
 	fin.close()
+	fout.close()
 
 main()
